8ad83bc6a431486a67d95ab20f8b0ddf16df3bf9
Benjamin Martin <ben.w.martin17@gmail.com>
2023-10-19 17:02:27 -0400

    added logs for each sale. All cones are selling

--- js/while.js
+++ js/while.js
@@ -15,17 +15,19 @@ do {
     conesPerCustomer = Math.floor(Math.random() * (5 - 1 + 1) ) + 1;
     if (totalCones > conesPerCustomer) { //
         console.log(`You asked for ${conesPerCustomer} cones. There are ${totalCones - conesPerCustomer} left.`);
-    }
-    if (conesPerCustomer > totalCones) { // breaks loop
+        totalCones -= conesPerCustomer;
+    } else if (conesPerCustomer > totalCones && totalCones !== 0) { // breaks loop
         console.log(`Sorry, you can't have ${conesPerCustomer} because there are only ${totalCones} left for the day.`);
-        break;
-    }
-    if (totalCones === conesPerCustomer) {
+        // } else if (totalCones === 0) {
+        //     console.log(`You need ${conesPerCustomer} and we have ${totalCones} left. Congrats! All cones are sold!`);
+        //     totalCones -= conesPerCustomer;
+        // }
+    } else {
         console.log(`You need ${conesPerCustomer} and we have ${totalCones} left. Congrats! All cones are sold!`);
-        break;
+        totalCones -= conesPerCustomer;
     }
-    totalCones -= conesPerCustomer;
-} while (totalCones >= 0);
+
+} while (totalCones > 0);
 
 
 
