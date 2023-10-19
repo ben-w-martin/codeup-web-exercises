c99b5ab5da3fc061ed07a6aabe2c07acba80c579
Benjamin Martin <ben.w.martin17@gmail.com>
2023-10-19 17:18:27 -0400

    cleaned up code. Made wording clearer

--- js/while.js
+++ js/while.js
@@ -9,19 +9,15 @@ while (i <= 16) {
     i++;
 }
 
-let totalCones = Math.floor(Math.random() * (100 - 50 + 1) ) + 50;
+let totalCones = Math.floor(Math.random() * (100 - 50 + 1)) + 50;
 let conesPerCustomer;
 do {
-    conesPerCustomer = Math.floor(Math.random() * (5 - 1 + 1) ) + 1;
+    conesPerCustomer = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
     if (totalCones > conesPerCustomer) { //
         console.log(`You asked for ${conesPerCustomer} cones. There are ${totalCones - conesPerCustomer} left.`);
         totalCones -= conesPerCustomer;
     } else if (conesPerCustomer > totalCones && totalCones !== 0) { // breaks loop
         console.log(`Sorry, you can't have ${conesPerCustomer} because there are only ${totalCones} left for the day.`);
-        // } else if (totalCones === 0) {
-        //     console.log(`You need ${conesPerCustomer} and we have ${totalCones} left. Congrats! All cones are sold!`);
-        //     totalCones -= conesPerCustomer;
-        // }
     } else {
         console.log(`You need ${conesPerCustomer} and we have ${totalCones} left. Congrats! All cones are sold!`);
         totalCones -= conesPerCustomer;
