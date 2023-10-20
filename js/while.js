74a8ecf3ac298621ad47e9cc48b512f95501d007
Benjamin Martin <ben.w.martin17@gmail.com>
2023-10-20 13:01:43 -0400

    completed array exercise

--- js/while.js
+++ js/while.js
@@ -1,26 +1,27 @@
 "use strict";
 
-let i = 1;
-let product = 1;
+(() => {
+    let i = 1;
+    let product = 1;
 
-while (i <= 16) {
-    product = product * 2;
-    console.log(product);
-    i++;
-}
-
-let totalCones = Math.floor(Math.random() * (100 - 50 + 1)) + 50;
-let conesPerCustomer;
-do {
-    conesPerCustomer = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
-    if (totalCones > conesPerCustomer) { // prevents total cones going into negatives
-        console.log(`There are ${totalCones}. You get ${conesPerCustomer} cone[s], leaving ${totalCones - conesPerCustomer}`);
-        totalCones -= conesPerCustomer;
-    } else if (conesPerCustomer > totalCones && totalCones !== 0) { //
-        console.log(`Sorry, you can't have ${conesPerCustomer} because there are/is only ${totalCones} left.`);
-    } else {
-        console.log(`You need ${conesPerCustomer} and we have ${totalCones} left. Congrats! All cones are sold!`);
-        totalCones -= conesPerCustomer;
+    while (i <= 16) {
+        product = product * 2;
+        console.log(product);
+        i++;
     }
-} while (totalCones > 0);
 
+    let totalCones = Math.floor(Math.random() * (100 - 50 + 1)) + 50;
+    let conesPerCustomer;
+    do {
+        conesPerCustomer = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+        if (totalCones > conesPerCustomer) { // prevents total cones going into negatives
+            console.log(`There are ${totalCones}. You get ${conesPerCustomer} cone[s], leaving ${totalCones - conesPerCustomer}`);
+            totalCones -= conesPerCustomer;
+        } else if (conesPerCustomer > totalCones && totalCones !== 0) { //
+            console.log(`Sorry, you can't have ${conesPerCustomer} because there are/is only ${totalCones} left.`);
+        } else {
+            totalCones -= conesPerCustomer;
+            console.log(`You need ${conesPerCustomer} and leaving us with ${totalCones}. Congrats! All cones are sold!`);
+        }
+    } while (totalCones > 0);
+})();
