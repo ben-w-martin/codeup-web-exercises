87f122fd9707e3c000822a10deed52bd5e5049c6
Benjamin Martin <ben.w.martin17@gmail.com>
2023-11-07 15:59:25 -0500

    moved jumbotron into container

---
+++ js/pizza.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const nextButton = document.getElementById("next-btn1");
+const sizeTab = document.getElementById("size-and-crust-tab");
+const cheeseTab = document.getElementById("cheese-and-sauce-tab");
+const toppingsTab = document.getElementById("toppings-tab");
+
+nextButton.onclick = function (){
+    const
+}
