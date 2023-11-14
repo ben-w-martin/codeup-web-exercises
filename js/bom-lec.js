6e1a9629d47ab116f78e4d03c37427df56878de6
Benjamin Martin <ben.w.martin17@gmail.com>
2023-11-14 12:30:30 -0500

    defused the BOM

---
+++ js/bom-lec.js
@@ -0,0 +1,23 @@
+"use strict";
+
+(() => {
+    // window.alert("JS is here");
+
+    // setTimeout(function () {
+    //     alert("You'll see this after 5 seconds.");
+    // }, 5000)
+    //
+    // let count = 0;
+    //
+    // const intervalId = setInterval(function () {
+    //     alert("You'll see this every 8 seconds.");
+    //     count++;
+    //     console.log(count);
+    //     if (count === 3) {
+    //         clearInterval(intervalId);
+    //     }
+    // }, 8000);
+
+    window.location = "https://google.com";
+
+})();
