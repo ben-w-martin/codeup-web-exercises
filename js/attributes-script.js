1a1827024bcb8fd13cb8a42f70f1890da76e10ba
Benjamin Martin <ben.w.martin17@gmail.com>
2023-11-15 12:51:36 -0500

    finished bonus ?

--- js/attributes-script.js
+++ js/attributes-script.js
@@ -27,32 +27,40 @@
     const toggleBtn = document.querySelector("#toggle-btn");
     const rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
-
-    setTimeout(function () {
-        profilePic.setAttribute("src", "../images/github-icon.png");
-    }, 2000);
-
-    setTimeout(function () {
-        profileName.innerHTML = "The Donut Man";
-    }, 4000);
-
-    setTimeout(function () {
-        profileDesc.style.color = "orangered";
-        profileDesc.style.fontFamily = "'nove', sans-serif";
-    }, 6000);
-
-
-    const changeBg = () => {
-        console.log("hello changeBg");
-        setInterval(function () {
-
-            profileCard.classList.toggle("green");
-        }, 2000);
+    function getRandomInt(min, max) {
+        if (typeof min === "boolean" || typeof max === "boolean" ||
+            min === null || max === null) {
+            return NaN;
+        }
+        return Math.floor((Math.random() * (max - min + 1)) + min);
     }
 
-    toggleBtn.addEventListener("click", changeBg);
+    const changeContent = () => {
+        let count = 0;
+        let intervalId = setInterval(function () {
+            count++;
+            profileCard.classList = [];
+            profileCard.classList.add(rainbow[getRandomInt(0, rainbow.length - 1)]);
+            switch (count) {
+                case 1:
+                    return profilePic.setAttribute("src", "../images/github-icon.png");
+                case 2:
+                    return profileName.innerHTML = "The Donut Man";
+                case 3:
+                    return profileDesc.style.color = "orangered",
+                        profileDesc.style.fontFamily = "'nove', sans-serif";
+                case 4:
+                    const newProfileName = prompt("Pick a new name!");
+                    return profileName.innerText = newProfileName;
+                default:
+                    break;
+            }
 
+        }, 2000);
 
+    }
+
+    toggleBtn.addEventListener("click", changeContent);
 
 
 })();
