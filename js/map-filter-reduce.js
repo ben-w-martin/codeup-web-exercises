c9f9a7df2515b71279d66b00beaaa8bf8e40138d
Benjamin Martin <ben.w.martin17@gmail.com>
2023-11-28 12:09:38 -0500

    remove unnecessary console.logs

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -79,7 +79,6 @@
     // todo Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
 
     const userNames = users.reduce((names, currentUser) => {
-        console.log(names);
         names.push(currentUser.name);
         return names;
     }, []).join(", ") + ".";
