44ed824fe8f76d95d667f903a301aff4899f1ed4
Benjamin Martin <ben.w.martin17@gmail.com>
2023-11-29 11:17:11 -0500

    testing fetch ability to read most recent commit

--- js/fetch.js
+++ js/fetch.js
@@ -1,7 +1,30 @@
 "use strict";
 
 (() => {
-    // import GH_KEY from "./keys";
+    const myUsername = "ben-w-martin";
+    const mostRecentCommitDate = getInfo(myUsername);
 
-    fetch("https://api.github.com/users/{username}/events/public'", {headers: {'Authorization': `token ${GH_KEY}`}}).then(response => response.json()).then(data => console.log(data)).catch(er => console.log("Error: ", er));
+    function getInfo(username) {
+        fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': `token ${GH_KEY}`}}).then(response => response.json()).then(data => {
+            console.log(data);
+            const commitsByDate = [];
+            data.forEach(item => {
+                if ("commits" in item.payload) {
+                    commitsByDate.push(item.created_at);
+                }
+            });
+            commitsByDate.sort((a, b) => b - a);
+            console.log(commitsByDate[0]);
+            return commitsByDate[0];
+        }).catch(er => console.log("Error: ", er));
+    }
+
+    const displayCommitDate = (date) => {
+        const dateToDisplay = document.createElement("p");
+        dateToDisplay.innerText = date;
+        document.querySelector("#commit-message").appendChild(dateToDisplay);
+    }
+
+    displayCommitDate(mostRecentCommitDate);
+
 })();
