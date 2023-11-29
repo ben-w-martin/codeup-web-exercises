ffe840fdfa7461cdc377d10bb2bcc89483cc8049
Benjamin Martin <ben.w.martin17@gmail.com>
2023-11-29 11:36:59 -0500

    function displays date of latest commit

--- js/fetch.js
+++ js/fetch.js
@@ -2,29 +2,25 @@
 
 (() => {
     const myUsername = "ben-w-martin";
-    const mostRecentCommitDate = getInfo(myUsername);
 
     function getInfo(username) {
+        const commitsByDate = [];
+        let date = "";
         fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization': `token ${GH_KEY}`}}).then(response => response.json()).then(data => {
-            console.log(data);
-            const commitsByDate = [];
             data.forEach(item => {
                 if ("commits" in item.payload) {
                     commitsByDate.push(item.created_at);
                 }
             });
             commitsByDate.sort((a, b) => b - a);
-            console.log(commitsByDate[0]);
-            return commitsByDate[0];
+            date = commitsByDate[0];
+        }).then(displayCommitDate => {
+            const dateToDisplay = document.createElement("p");
+            dateToDisplay.innerText = date;
+            document.querySelector("#commit-message").appendChild(dateToDisplay)
         }).catch(er => console.log("Error: ", er));
     }
 
-    const displayCommitDate = (date) => {
-        const dateToDisplay = document.createElement("p");
-        dateToDisplay.innerText = date;
-        document.querySelector("#commit-message").appendChild(dateToDisplay);
-    }
-
-    displayCommitDate(mostRecentCommitDate);
+    getInfo(myUsername);
 
 })();
