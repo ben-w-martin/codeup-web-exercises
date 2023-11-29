6979666856b8713d3b2e9f17cb303de9ceae3320
Benjamin Martin <ben.w.martin17@gmail.com>
2023-11-28 23:01:48 -0500

    working on handling API promise

--- js/fetch.js
+++ js/fetch.js
@@ -1,5 +1,7 @@
 "use strict";
-(()=>{
-console.log(GH_KEY);
 
+(() => {
+    // import GH_KEY from "./keys";
+
+    fetch("https://api.github.com/users/{username}/events/public'", {headers: {'Authorization': `token ${GH_KEY}`}}).then(response => response.json()).then(data => console.log(data)).catch(er => console.log("Error: ", er));
 })();
