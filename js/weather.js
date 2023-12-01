67e0c502494b8900904c88d63b129092814a383b
Benjamin Martin <ben.w.martin17@gmail.com>
2023-12-01 12:59:19 -0500

    display city

--- js/weather.js
+++ js/weather.js
@@ -5,10 +5,33 @@
     // render map------------------------------------------------------------------
     mapboxgl.accessToken = MB_KEY;
     const coordinates = document.getElementById('coordinates');
-    var map = new mapboxgl.Map({
-        container: mapContainer,
-        style: 'mapbox://styles/mapbox/streets-v11'
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/satellite-streets-v12',
+        center: [-79.1, 37.4],
+        zoom: 3
     });
+
+    const marker = new mapboxgl.Marker({
+        draggable: true
+    })
+        .setLngLat([-79.1, 37.4])
+        .addTo(map);
+
+    async function onDragEnd() {
+        const lngLat = marker.getLngLat();
+        let city = await reverseGeocode(lngLat, MB_KEY);
+        coordinates.style.display = 'block';
+        coordinates.innerHTML = `Longitude: ${lngLat.lng}<br />Latitude: ${lngLat.lat}`;
+        renderCity(city);
+    }
+
+    function renderCity(city) {
+        let currentCity = document.querySelector(".city");
+        currentCity.innerText = city;
+    }
+
+    marker.on('dragend', onDragEnd);
 
     map.addControl(new mapboxgl.NavigationControl());
 
@@ -16,31 +39,17 @@
         var baseUrl = 'https://api.mapbox.com';
         var endPoint = '/geocoding/v5/mapbox.places/';
         return fetch(`${baseUrl}${endPoint}${encodeURIComponent(search)}.json?access_token=${token}`)
-            .then( res => res.json() )
-            // to get all the data from the request, comment out the following three lines...
-            .then( data => data.features[0].center);
+            .then(res => res.json())
+            .then(data => data.features[0].center);
     }
 
-    function reverseGeocode(coordinates, token) {
+    async function reverseGeocode(coordinates, token) {
         var baseUrl = 'https://api.mapbox.com';
         var endPoint = '/geocoding/v5/mapbox.places/';
-        return fetch(`${baseUrl}${endPoint}${coordinates.lng},${coordinates.lat}.json?access_token=${token}`)
-            .then( res => res.json() )
-            // to get all the data from the request, comment out the following three lines...
-            .then( data => data.features[0].place_name );
+        const response = await fetch(`${baseUrl}${endPoint}${coordinates.lng},${coordinates.lat}.json?access_token=${token}`)
+            .then(res => res.json());
+        // console.log(response.features);
+        return `${response.features[2].text}, ${response.features[4].text}`;
     }
 
-    const marker = new mapboxgl.Marker({
-        draggable: true
-    })
-        .setLngLat([0, 0])
-        .addTo(map);
-
-    function onDragEnd() {
-        const lngLat = marker.getLngLat();
-        coordinates.style.display = 'block';
-        coordinates.innerHTML = `Longitude: ${lngLat.lng}<br />Latitude: ${lngLat.lat}`;
-    }
-
-    marker.on('dragend', onDragEnd);
 })();
